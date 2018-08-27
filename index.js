458b51741bffa7b2deb5f6b8585ac46f55e1e2a0
Wyatt Ades <wyattades@gmail.com>
2018-08-27 16:12:59 -0700

    Prefer web workers

--- index.js
+++ index.js
@@ -2,6 +2,8 @@ const $error = document.getElementById('error');
 const $input = document.getElementById('input');
 const $output = document.getElementById('output');
 const $progress = document.getElementById('progress');
+let worker;
+window.Worker = null;
 
 const editorOptions = {
   tabSize: 2,
@@ -23,8 +25,13 @@ const outEditor = CodeMirror.fromTextArea($output, Object.assign({
 }, editorOptions));
 
 const updateProgress = (progress) => {
-  if (progress === 0) progress = 0.4;
-  $progress.style.width = (progress * 100) + '%';
+  if (progress === 0) {
+    $progress.classList.add('easing', 'start');
+    $progress.style.width = '30%';
+  } else {
+    $progress.classList.remove('start');
+    $progress.style.width = (progress * 100) + '%';
+  }
 };
 
 let options = {};
@@ -34,25 +41,73 @@ try {
 } catch(_) {}
 
 
-const convert = () => {
-  const javaString = inEditor.getValue();
-  if (javaString) {
-    try {
-      const jsString = window.javaToJavascript(javaString, options, updateProgress);
-      outEditor.setValue(jsString);
-      $error.classList.remove('active');
-    } catch(e) {
-      // console.error(e);
-      $error.classList.add('active');
-      if (e.name === 'SyntaxError') $error.innerText = `SyntaxError around line ${e.location.start.line}: ${e.message}`;
-      else $error.innerText = `Error: ${e}`;
-    }
-  } else {
-    outEditor.setValue('');
+const handleError = (e) => {
+  $progress.classList.add('error');
+  $error.classList.add('active');
+  if (e.name === 'SyntaxError') $error.innerText = `SyntaxError around line ${e.location.start.line}: ${e.message}`;
+  else $error.innerText = `Error: ${e}`;
+};
+
+const setResult = (str) => {
+  outEditor.setValue(str);
+  $error.classList.remove('active');  
+
+  if (!str) {
+    $progress.classList.remove('easing', 'start');
     $progress.style.width = 0;
+    // setTimeout(() => $progress.classList.add('easing'));
   }
 };
-convert();
+
+let converting = false;
+
+const convert = () => {
+  $error.classList.remove('active');
+  $progress.classList.remove('error', 'easing', 'start');
+  $progress.style.width = 0;
+
+  const javaString = inEditor.getValue().trim();
+  if (javaString) {
+    if (worker) {
+      converting = true;
+      worker.postMessage([ 'convert', javaString, options ]);
+    } else {
+      try {
+        setResult(window.javaToJavascript(javaString, options, updateProgress));
+      } catch(e) {
+        handleError(e);
+      }
+    }
+  } else {
+    setResult('');
+  }
+};
+
+const setWorker = () => {
+  if (worker) worker.terminate();
+  worker = new Worker('worker.js');
+  worker.onmessage = ({ data: [ cmd, data ] }) => {
+    if (cmd === 'progress') updateProgress(data);
+    else if (cmd === 'result') setResult(data);
+    else if (cmd === 'error') handleError(data);
+
+    if (cmd === 'result' || cmd === 'error') {
+      converting = false;
+    }
+  };
+}
+
+if (window.Worker) {
+  setWorker();
+  convert();
+} else {
+  // Synchronously load java-to-javascript
+  const s = document.createElement('script');
+  s.onload = () => convert();
+  s.src = 'https://unpkg.com/java-to-javascript@latest/build/java-to-javascript.min.js';
+  s.type = 'text/javascript';
+  document.head.appendChild(s);
+}
 
 const updateOptions = (e) => {
   const el = e.target;
@@ -74,7 +129,14 @@ document.querySelectorAll('#menu input').forEach((el) => {
   el.addEventListener('change', updateOptions);
 });
 
+let timeout;
 inEditor.on('change', () => {
-  window.localStorage.setItem('java2js-editor', inEditor.getValue());
-  convert();
+  if (timeout) clearTimeout(timeout);
+  timeout = setTimeout(() => {
+    window.localStorage.setItem('java2js-editor', inEditor.getValue());
+    if (worker && converting) {
+      setWorker();
+    }
+    convert();
+  }, 1000);
 });
---
+++ worker.js
@@ -0,0 +1,14 @@
+importScripts('https://unpkg.com/java-to-javascript@latest/build/java-to-javascript.min.js');
+
+self.onmessage = ({ data: [ cmd, data, options ] }) => {
+  if (cmd === 'convert') {
+    try {
+      const result = self.javaToJavascript(data, options, (progress) => {
+        self.postMessage([ 'progress', progress ]);
+      });
+      self.postMessage([ 'result', result ]);
+    } catch (e) {
+      self.postMessage([ 'error', e ]);
+    }
+  }
+};
